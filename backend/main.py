e69fea7f9649d1fc1f54246671222f83e4b0e1a8
hminshen <hminshen@gmail.com>
2024-02-04 15:37:26 +0800

    feature: added file to get answer from bard api

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+import bardapi
+
+# set your __Secure-1PSID value to key
+token = 'g.a000gAgEzZBLKxtQSP0NRChq3ujkg-e7_zUkfQUrxBzd5nyuYnuxAhsSa_5o5g-sEymt1ykl2AACgYKAUQSAQASFQHGX2MiX0mwBk2scDnklqG69RgQpRoVAUF8yKqqjwVlpcKIsXLCxDgrhday0076'
+
+# set your input text
+input_text = "what is python used for?"
+print(input_text)
+
+# Send an API request and get a response.
+response = bardapi.core.Bard(token).get_answer(input_text)
+print(response)
