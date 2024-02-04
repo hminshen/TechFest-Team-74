214a15b4f68a4a3c07d0a284024ec43ccc7fc67c
Hongyi000 <hongyishy@gmail.com>
2024-02-04 15:49:37 +0800

    added print

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ import bardapi
 
 # set your __Secure-1PSID value to key
 token = 'g.a000gAgEzZBLKxtQSP0NRChq3ujkg-e7_zUkfQUrxBzd5nyuYnuxAhsSa_5o5g-sEymt1ykl2AACgYKAUQSAQASFQHGX2MiX0mwBk2scDnklqG69RgQpRoVAUF8yKqqjwVlpcKIsXLCxDgrhday0076'
-
+print ("test")
 # set your input text
 input_text = "what is python used for?"
 print(input_text)
