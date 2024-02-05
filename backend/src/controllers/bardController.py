83863a37ac2d731127c44203c089aa1c3e6c2e62
hminshen <hminshen@gmail.com>
2024-02-06 00:51:32 +0800

    Feat: Added feedback code

--- backend/src/controllers/bardController.py
+++ backend/src/controllers/bardController.py
@@ -1,6 +1,28 @@
 from services.bardServices import BardService
 from flask import request, jsonify
 
+
+def extract_pre_interview_info(pre_interview_info):
+    # Extract values from the JSON data
+    job_type = "\nJob Type: " + pre_interview_info.get('job_type')
+    experience_level = "\nExperience Level: " + pre_interview_info.get('experience_level')
+    skills = "\nSkills: " + str(pre_interview_info.get('skills', []))
+    job_description = "\nJob Description: " + pre_interview_info.get('job_description')
+    resume = "\nResume: " + pre_interview_info.get('resume')
+    cover_letter = "\nCover Letter: " + pre_interview_info.get('cover_letter')
+    focus_areas = "\nFocus Areas: " + str(pre_interview_info.get('focus_areas', []))
+    interview_info = "\n--Start Of Info--\n" + job_type + experience_level + skills + job_description + resume + cover_letter + focus_areas + "\n--End Of Info--\n"
+    return interview_info
+
+def extract_interview_qns_ans(interview_answers):
+    # Extract values from the JSON data
+    interview_results = ""
+    print(interview_answers)
+    for question in interview_answers:
+      interview_results = interview_results + "Question: " + question + ", Answer: " + str(interview_answers[question]) + "\n"
+    print(interview_results)
+    return interview_results
+
 class BardController:
     @staticmethod
     def get_questions():
@@ -8,16 +30,7 @@ class BardController:
         # pre_interview_info = request.get_data().decode("utf-8")
         # Get JSON data from the request
         pre_interview_info = request.get_json()
-
-        # Extract values from the JSON data
-        job_type = "\nJob Type: " + pre_interview_info.get('job_type')
-        experience_level = "\nExperience Level: " + pre_interview_info.get('experience_level')
-        skills = "\nSkills: " + str(pre_interview_info.get('skills', []))
-        job_description = "\nJob Description: " + pre_interview_info.get('job_description')
-        resume = "\nResume: " + pre_interview_info.get('resume')
-        cover_letter = "\nCover Letter: " + pre_interview_info.get('cover_letter')
-        focus_areas = "\nFocus Areas: " + str(pre_interview_info.get('focus_areas', []))
-        interview_info = "\n--Start Of Info--\n" + job_type + experience_level + skills + job_description + resume + cover_letter + focus_areas + "\n--End Of Info--\n"
+        interview_info = extract_pre_interview_info(pre_interview_info)
         
         # Target Format of the Response:
         json_format_res = """\n\nCan you give me the questions in the form of json format? I want the answer that you provide me to only follow this format:\n{
@@ -42,8 +55,25 @@ class BardController:
         return jsonify(data)
     
     @staticmethod
-    def get_feedback(input_text):
-        input_text = request.form['prompt']
-        print("Question:", input_text)
-        data = BardService.get_bard_query(input_text)
+    def get_feedback():
+        # Get JSON data from the request
+        interview_request = request.get_json()
+
+        pre_interview_info = interview_request['pre_info']
+        print(pre_interview_info)
+        pre_interview_context = extract_pre_interview_info(pre_interview_info)
+        print(pre_interview_context)
+
+        interview_answers = interview_request['interview_answers']
+        interview_results = extract_interview_qns_ans(interview_answers)
+
+        # Crafting the prompt:
+        before = "I am conducting a mock job interview for myself, this is the information that i have gathered for myself as stated below:\n"
+        middle = "\nBased on this information above, these are the questions that you suggested i should tackle for my interview, as well as my responses to the questions. These are stated below:\n"
+        after = "\nCan you provide me some honest feedback for how i can better improve my interview?"
+
+        post_interview_prompt =  before + pre_interview_context + middle + interview_results + after
+        print(post_interview_prompt)
+
+        data = BardService.get_bard_query(post_interview_prompt)
         return jsonify(data)
